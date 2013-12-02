c02550e685b55de17b3e2c89e86b3ebca7c94674
Winner Crespo <wistcc@hotmail.com>
2013-12-02 10:13:35 -0400

    Adding the project to GitHub

---
+++ LetsDrawTogether/HubController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace LetsDrawTogether
+{
+    public class HubController : Hub
+    {
+        public async Task JoinGroup(string groupId = "1")
+        {
+            await Groups.Add(Context.ConnectionId, "Group" + groupId);
+        }
+
+        public void DrawTogether(string res, int clientX, int clientY, string groupId = "1")
+        {
+            Clients.OthersInGroup("Group" + groupId).drawTogether(res,clientX, clientY);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Let's Draw Together
+===========
+
+The project allows you draw with a remote partner and have fun. Please fork and contribute.
